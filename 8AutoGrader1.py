06ee8c5c2cf531a6d58eaeb47358e31734a96976
matthewmedcalf96 <46378096+matthewmedcalf96@users.noreply.github.com>
2020-11-13 17:21:55 -0600

    Update 8AutoGrader1.py

--- 8AutoGrader1.py
+++ 8AutoGrader1.py
@@ -2,11 +2,13 @@ fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
-    wrd=line.rstrip().split()
-    for element in wrd:
-    	if element in lst:
-        	continue
-        else:
-        	lst.append(element)
+	line = line.rstrip()
+	words = line.split()
+	for element in words :
+		if element in lst:
+			continue
+		else :
+			lst.append(element)
+
 lst.sort()
-print(lst
+print(lst)
